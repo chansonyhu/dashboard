73b40a16efb1944559a91c21207fd9c55f2fb96d
于千山 <yqs17@mails.tsinghua.edu.cn>
2019-07-15 10:29:07 +0800

    minor

--- src/assets/scripts/vectorMaps/index.js
+++ src/assets/scripts/vectorMaps/index.js
@@ -23,68 +23,69 @@ export default (function () {
         >
         </div>
       `);
-
-      $('#vmap').vectorMap({
-        map: 'world_mill',
-        backgroundColor: '#fff',
-        borderColor: '#fff',
-        borderOpacity: 0.25,
-        borderWidth: 0,
-        color: '#e6e6e6',
-        regionStyle : {
-          initial : {
-            fill : '#e4ecef',
-          },
-        },
-
-        markerStyle: {
-          initial: {
-            r: 7,
-            'fill': '#fff',
-            'fill-opacity':1,
-            'stroke': '#000',
-            'stroke-width' : 2,
-            'stroke-opacity': 0.4,
-          },
-        },
-
-        markers : [{
-          latLng : [21.00, 78.00],
-          name : 'INDIA : 350',
-        }, {
-          latLng : [-33.00, 151.00],
-          name : 'Australia : 250',
-        }, {
-          latLng : [36.77, -119.41],
-          name : 'USA : 250',
-        }, {
-          latLng : [55.37, -3.41],
-          name : 'UK   : 250',
-        }, {
-          latLng : [25.20, 55.27],
-          name : 'UAE : 250',
-        }],
-        series: {
-          regions: [{
-            values: {
-              'US': 298,
-              'SA': 200,
-              'AU': 760,
-              'IN': 200,
-              'GB': 120,
+      $.getJSON('assets/static/data/login_country.json', function(data){
+        $('#vmap').vectorMap({
+          map: 'world_mill',
+          backgroundColor: '#fff',
+          borderColor: '#fff',
+          borderOpacity: 0.25,
+          borderWidth: 0,
+          color: '#e6e6e6',
+          regionStyle : {
+            initial : {
+              fill : '#e4ecef',
             },
-            scale: ['#03a9f3', '#02a7f1'],
-            normalizeFunction: 'polynomial',
-          }],
-        },
-        hoverOpacity: null,
-        normalizeFunction: 'linear',
-        zoomOnScroll: false,
-        scaleColors: ['#b6d6ff', '#005ace'],
-        selectedColor: '#c9dfaf',
-        selectedRegions: [],
-        enableZoom: false,
-        hoverColor: '#fff',
+          },
+
+          markerStyle: {
+            initial: {
+              r: 7,
+              'fill': '#fff',
+              'fill-opacity':1,
+              'stroke': '#000',
+              'stroke-width' : 2,
+              'stroke-opacity': 0.4,
+            },
+          },
+
+          // markers : [{
+          //   latLng : [21.00, 78.00],
+          //   name : 'INDIA : 350',
+          // }, {
+          //   latLng : [-33.00, 151.00],
+          //   name : 'Australia : 250',
+          // }, {
+          //   latLng : [36.77, -119.41],
+          //   name : 'USA : 250',
+          // }, {
+          //   latLng : [55.37, -3.41],
+          //   name : 'UK   : 250',
+          // }, {
+          //   latLng : [25.20, 55.27],
+          //   name : 'UAE : 250',
+          // }],
+          series: {
+            regions: [{
+              values: data,
+              scale: ['#C8EEFF', '#0071A4'],
+              normalizeFunction: 'polynomial',
+            }],
+          },
+          onRegionTipShow: function(event, label, code){
+            label.html(
+              '<b>'+label.html()+'</b></br>'+
+              '<b>登录数: </b>'+ data[code] // typeof(data[code]) === "undefined" ? "0" : data[code]
+            );
+          },
+          hoverOpacity: null,
+          normalizeFunction: 'linear',
+          zoomOnScroll: false,
+          scaleColors: ['#b6d6ff', '#005ace'],
+          selectedColor: '#c9dfaf',
+          selectedRegions: [],
+          enableZoom: false,
+          hoverColor: '#fff',
+        });
       });
     }
   };
