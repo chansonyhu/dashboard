9cfd1b9858df685d6e858cd9c8a2e33cc8c9d520
yqs17 <yqs17@mails.tsinghua.edu.cn>
2019-08-05 03:30:39 +0000

    sync: cannot redirect

--- src/assets/scripts/signin/index.js
+++ src/assets/scripts/signin/index.js
@@ -1,63 +1,91 @@
 import mysql from 'mysql';
-import express from 'express';
-import session from 'express-session';
-import bodyParser from 'body-parser';
-import path from 'path';
+// import express from 'express';
+// import session from 'express-session';
+// import bodyParser from 'body-parser';
+// import path from 'path';
 
 
-console.log(path.join(__dirname + '/signin.html'));
+// console.log(path.join(__dirname + '/signin.html'));
 
-var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : '941216yqs',
-	database : 'nodelogin'
-});
+// var connection = mysql.createConnection({
+// 	host     : 'localhost',
+// 	user     : 'root',
+// 	password : '941216yqs',
+// 	database : 'nodelogin'
+// });
 
-console.log('express start');
-var app = express();
-console.log('express finish');
-app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
-}));
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
+// console.log('express start');
+// var app = express();
+// console.log('express finish');
+// app.use(session({
+// 	secret: 'secret',
+// 	resave: true,
+// 	saveUninitialized: true
+// }));
+// app.use(bodyParser.urlencoded({extended : true}));
+// app.use(bodyParser.json());
 
-app.get('/', function(request, response) {
-	console.log(path.join(__dirname + '/signin.html'));
-	response.sendFile(path.join(__dirname + '/signin.html'));
-});
+// app.get('/', function(request, response) {
+// 	console.log(path.join(__dirname + '/signin.html'));
+// 	response.sendFile(path.join(__dirname + '/signin.html'));
+// });
 
-app.post('/auth', function(request, response) {
-	var username = request.body.username;
-	var password = request.body.password;
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+
+function validate() {
+
+	// var username = document.getElementById("username").value;
+	// var password = document.getElementById("password").value;
+	// if ( username == "Formget" && password == "formget#123"){
+	// alert ("Login successfully");
+	// window.location = "success.html"; // Redirecting to other page.
+	// return false;
+	// }
+	// else{
+	// attempt --;// Decrementing by one.
+	// alert("You have left "+attempt+" attempt;");
+	// // Disabling fields after 3 attempts.
+	// if( attempt == 0){
+	// document.getElementById("username").disabled = true;
+	// document.getElementById("password").disabled = true;
+	// document.getElementById("submit").disabled = true;
+	// return false;
+	// }
+	// }
+
+	console.log('validate function');
+
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
 	console.log(username, '\n', password);
 	if (username && password) {
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				response.redirect('/index.html');
-			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
-});
+		// window.location = "index.html"; 
 
-app.get('/index.html', function(request, response) {
-	if (request.session.loggedin) {
-		response.send('Welcome back, ' + request.session.username + '!');
+		// connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// if (results.length > 0) {
+				// window.location = "index.html"; 
+				// request.session.loggedin = true;
+				// request.session.username = username;
+				// response.redirect('/index.html');
+			// } else {
+				// alert('Incorrect Username and/or Password!');
+			// }			
+			// response.end();
+		// });
 	} else {
-		response.send('Please login to view this page!');
+		alert('Please enter Username and Password!');
+		// response.end();
 	}
-	response.end();
-});
+};
 
-app.listen(30000);
+// app.get('/index.html', function(request, response) {
+// 	if (request.session.loggedin) {
+// 		response.send('Welcome back, ' + request.session.username + '!');
+// 	} else {
+// 		response.send('Please login to view this page!');
+// 	}
+// 	response.end();
+// });
+
+// app.listen(30000);
