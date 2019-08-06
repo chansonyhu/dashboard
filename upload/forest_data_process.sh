3fa0fef898ed83a9c9dd85a5047a025e04dd08d1
yqs17 <yqs17@mails.tsinghua.edu.cn>
2019-08-06 17:57:40 +0000

    driver script

---
+++ upload/forest_data_process.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+
+WORK_PATH="/home/"$USER"/dashboard/upload/DataDeal"
+EXCEL_PATH="$WORK_PATH/data/excel"
+cd $EXCEL_PATH
+file=$1
+if [ "${file##*.}"x = "xlsx"x ]||[ "${file##*.}"x = "xls"x ];then
+    mv $EXCEL_PATH/$file $EXCEL_PATH/../
+fi
+if [ "${file##*.}"x = "zip"x ];then
+    cd $EXCEL_PATH
+    unzip $file
+    rm $EXCEL_PATH/$file
+    dir=`ls -d */`
+    mv $EXCEL_PATH/$dir/* $EXCEL_PATH/../
+    rm -R $EXCEL_PATH/$dir 
+fi
+if [ "${file##*.}"x = "rar"x ];then
+    cd $EXCEL_PATH
+    unzip $file
+    rm $EXCEL_PATH/$file
+    dir=`ls -d */`
+    mv $EXCEL_PATH/$dir/* $EXCEL_PATH/../
+    rm -R $EXCEL_PATH/$dir 
+fi
+cd $WORK_PATH
+for script in `ls *.py`
+do
+    python3.6 $script
+done
+rm -R $WORK_PATH/data/excel/*
