3525ea2628d2209ff977b015c2ecc3d5f2c8f2b3
yqs17 <yqs17@mails.tsinghua.edu.cn>
2019-08-07 10:05:17 +0000

    trim

--- webpack/plugins/htmlPlugin.js
+++ webpack/plugins/htmlPlugin.js
@@ -10,19 +10,10 @@ const titles = {
   '02-repossession': '预约收楼',
   '03-app_payment': 'App支付',
   '04-reading': '阅森活',
-  'blank': 'Blank',
-  'buttons': 'Buttons',
-  'charts': 'Charts',
-  'datatable': 'Datatable',
-  'forms': 'Forms',
-  'google-maps': 'Google Maps',
   'signup': 'Signup',
   'login': 'Login',
-  'ui': 'UI',
-  'vector-maps': 'Vector Maps',
   '404': '404',
   '500': '500',
-  'basic-table': 'Basic Table',
 };
 
 module.exports = Object.keys(titles).map(title => {
