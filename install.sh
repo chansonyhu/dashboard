8454a31443eec6474670348314c5c346724488e5
chansonyhu <chansonyhu@gmail.com>
2019-08-08 21:09:43 -0700

    minor

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo apt-get install python3-pip unar unzip
+sudo apt-get update
+sudo apt-get install python3-pip unar unzip nodejs-legacy npm vim 
+cd ~/dashboard
 pip3 install xlrd pandas apt_pkg
 
