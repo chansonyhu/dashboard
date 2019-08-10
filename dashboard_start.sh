ba2c93bbf7c1739db55fff4de3b9e6ae7910efff
yqs17 <yqs17@mails.tsinghua.edu.cn>
2019-08-10 06:37:40 +0000

    start script

--- dashboard_start.sh
+++ dashboard_start.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+sudo service mongodb stop
+sudo service mysql stop
+# swap
+# sudo fallocate -l 1G /swapfile
+# sudo chmod 600 /swapfile
+# sudo mkswap /swapfile
+# sudo swapon /swapfile
+#======
+# sudo ln -s /usr/bin/nodejs /usr/bin/node
 cd /home/forest/dashboard
-sudo -u forest npm run preview & > dashboard.log 2>&1
+sudo -u forest npm run preview > dashboard.log 2>&1 &
 cd upload
-sudo -u forest node nodejs-upload-file.js & > ../upload.log 2>&1
-
+sudo -u forest node nodejs-upload-file.js > ../upload.log 2>&1 &
+# cd /home/chansonyhu123/code-server1.1119-vsc1.33.1-linux-x64/
+# sudo -u chansonyhu123 ./code-server -N -H -p 59999 &
+# chmod 440 /etc/sudoers
